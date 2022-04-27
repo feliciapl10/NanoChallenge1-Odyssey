2b2c4d5cbd489b6b4024eb50bdae1afa6065b1fb
Felicia <fprisqilla22@bsd.idserve.net>
2022-04-27 15:53:47 +0700

    Initial Commit

---
+++ Nano Challenge Travel App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nano Challenge Travel App
+//
+//  Created by Felicia Prisqilla on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
